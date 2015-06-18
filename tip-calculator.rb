674ba097fd89f5bb53661d31cb16ca2115f4b7a8
Dylan Steck <dylancsteck@gmail.com>
2015-06-18 18:09:57 +0000

    this is a tip calculator

--- tip-calculator.rb
+++ tip-calculator.rb
@@ -13,15 +13,62 @@ percent = gets.chomp
 puts "How many people are you splitting the bill with?"
 people = gets.chomp
 
-bills = bill.gsub(/[a-zA-Z]/, "").gsub("$", "").to_f
-percents = percent.gsub(/[a-zA-Z]/, "").gsub("%", "").to_f
-percentb = percents/100
-taxs = tax.gsub(/[a-zA-Z]/, "").to_f
-peoples = people.gsub(/[a-zA-Z]/, "").to_i
-tip = bills * percentb
-total = tip + taxs + bills
+# bills = bill.gsub(/[a-zA-Z]/, "").gsub("$", "").to_f
+# percents = percent.gsub(/[a-zA-Z]/, "").gsub("%", "").to_f
+# percentb = percents/100
+# taxs = tax.gsub(/[a-zA-Z]/, "").to_f
+# peoples = people.gsub(/[a-zA-Z]/, "").to_i
+# tip = bills * percentb
+# total = tip + taxs + bills
+# final = total/peoples
+# finals = (final*20).round / 20.000
 
-puts "The total per person (including tip) is: $#{total/peoples}"
+def bill_clean(bill) 
+	bill.gsub(/[a-zA-Z]/, "").gsub("$", "").to_f
+end
+bills = bill_clean(bill)
+
+def tax_clean(tax)
+	tax.gsub(/[a-zA-Z]/, "").to_f
+end
+taxs = tax_clean(tax)
+
+def percent_clean(percent)
+	percent.gsub(/[a-zA-Z]/, "").gsub("%", "").to_f
+end
+p = percent_clean(percent)
+
+def percent_into_dec(p)
+p/100
+end
+percents = percent_into_dec(p)
+
+def people_clean(people)
+	people.gsub(/[a-zA-Z]/, "").to_i
+end
+peoples = people_clean(people)
+
+def tip_total(bills, percents)
+	bills * percents
+end
+tip_amount = tip_total(bills, percents)
+
+def total(tip_amount, taxs, bills)
+	tip_amount + taxs + bills
+end
+totals = total(tip_amount, taxs, bills)
+
+def final(totals, peoples)
+	totals/peoples
+end
+finals = final(totals, peoples)
+
+def final_clean(finals)
+	(finals*20).round / 20.000
+end
+total_final = final_clean(finals)
+
+puts "The total per person (including tip) is: $#{total_final}"
 
 
 
